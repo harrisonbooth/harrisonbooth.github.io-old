889bca0d9ce9453c5aa972c1c5e150105706c408
Harrison Booth <harrisonbooth@ymail.com>
2017-07-25 14:50:03 +0100

    add click to email footer to copy email when clicked

---
+++ build/bundle.js
@@ -0,0 +1,101 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var windowOnLoad = function windowOnLoad() {
+  var emailContainer = document.getElementsByClassName("footer-email-container")[0];
+  var emailText = document.getElementById("footer-email-text");
+
+  emailContainer.addEventListener("click", function (event) {
+    var stringTextArea = document.createElement("textarea");
+    stringTextArea.style.position = "fixed";
+    stringTextArea.value = "harrisonboothh@gmail.com";
+
+    document.body.appendChild(stringTextArea);
+
+    stringTextArea.focus();
+    stringTextArea.select();
+    document.execCommand("copy");
+
+    document.body.removeChild(stringTextArea);
+    emailText.innerText = "Email coped to clipboard!";
+
+    setTimeout(function () {
+      emailText.innerText = "harrisonboothh@gmail.com";
+    }, 1000);
+  });
+};
+
+window.addEventListener("load", windowOnLoad);
+
+/***/ })
+/******/ ]);
+//# sourceMappingURL=bundle.js.map
--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const windowOnLoad = () => {
+  const emailContainer = document.getElementsByClassName("footer-email-container")[0]
+  const emailText = document.getElementById("footer-email-text")
 
+  emailContainer.addEventListener("click", (event) => {
+    var stringTextArea = document.createElement("textarea")
+    stringTextArea.style.position = "fixed"
+    stringTextArea.value = "harrisonboothh@gmail.com"
+
+    document.body.appendChild(stringTextArea)
+
+    stringTextArea.focus()
+    stringTextArea.select()
+    document.execCommand("copy")
+
+    document.body.removeChild(stringTextArea)
+    emailText.innerText = "Email coped to clipboard!"
+
+    setTimeout(() => {
+      emailText.innerText = "harrisonboothh@gmail.com"
+    }, 1000)
+  })
 }
 
 window.addEventListener("load", windowOnLoad)

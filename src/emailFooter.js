4aeebba76289f1d92122c4b99a8d2efa93c357bb
Harrison Booth <harrisonbooth@ymail.com>
2017-08-08 10:43:41 +0100

    Add semicolons

--- src/emailFooter.js
+++ src/emailFooter.js
@@ -1,22 +1,22 @@
 const setupCopyContainer = (container, element, text) => {
   container.addEventListener("click", (event) => {
-    var stringTextArea = document.createElement("textarea")
-    stringTextArea.style.position = "fixed"
-    stringTextArea.value = text
+    var stringTextArea = document.createElement("textarea");
+    stringTextArea.style.position = "fixed";
+    stringTextArea.value = text;
 
-    document.body.appendChild(stringTextArea)
+    document.body.appendChild(stringTextArea);
 
-    stringTextArea.focus()
-    stringTextArea.select()
-    document.execCommand("copy")
+    stringTextArea.focus();
+    stringTextArea.select();
+    document.execCommand("copy");
 
-    document.body.removeChild(stringTextArea)
-    element.innerText = "Email copied to clipboard!"
+    document.body.removeChild(stringTextArea);
+    element.innerText = "Email copied to clipboard!";
 
     setTimeout(() => {
-      element.innerText = "harrisonboothh@gmail.com"
-    }, 1000)
-  })
+      element.innerText = "harrisonboothh@gmail.com";
+    }, 1000);
+  });
 }
 
-export default setupCopyContainer
+export default setupCopyContainer;
